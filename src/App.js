e2bcec6b30082cef06891c0f459a3b1292f47c8a
jafar1405 <jafar1405@gmail.com>
2023-08-02 18:15:44 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       <button onClick={handleClick}>Click me</button>
       {/* <div>here: {showValue}</div> */}
       {/* properties to be called as "props" */}
-      <Output value={showValue} here="sff:" />
+      <Output value={showValue} here="here td sdf:" />
     </div>
   );
 }
